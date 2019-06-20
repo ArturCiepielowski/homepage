295972308831ef430b0650ddeb00dd3dff57d4f4
= <artciepi@wp.pl>
2019-06-20 15:23:04 +0200

    feat:improvements

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log('Siema!');
+
+
+console.log('.paragraph__test--js');
+
+
+const magicWord = document.querySelector('.paragraph__test--js');
+
+console.log(magicWord.innerHTML);
+
+magicWord.innerHTML='Magiczne słowo stworzone przez JavaScript'
+
+
+
