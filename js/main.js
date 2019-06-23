eaee87f3819e2bdf44d62debe96cda7f45bbfe64
= <artciepi@wp.pl>
2019-06-23 14:35:20 +0200

    feat:page improvements

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+
+document.getElementById("myButton--js").onclick=function(){
+
+var name=prompt('Cześć .... eee możesz powtórzyć jak masz na mię ?','');
+var age=prompt(`Witaj, ${name} powiedz mi proszę .. masz więcej niż 18 lat .. prawda ? `,'');
+
+if(age>=18){
+
+  alert('Super to idziemy na piwo!');
+
+} else if(age<18){
+
+    alert('No nic to niestety soczek dla ciebie');
+} else {
+
+    alert('Miałeś wpisać swoj wiek..... TYPIE!');
+}
+
+}
+
+
+const magicWord = document.querySelector('.paragraph__test--js');
+
+console.log(magicWord.innerHTML);
+
+magicWord.innerHTML='Magiczne słowo stworzone przez JavaScript'
+
+
+
