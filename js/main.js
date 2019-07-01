3e26f5f76d1252bbfaa8378ef1ebefba53ec5874
= <artciepi@wp.pl>
2019-07-01 22:46:53 +0200

    Hamburger Menu

--- js/main.js
+++ js/main.js
@@ -28,3 +28,12 @@ magicWord.innerHTML='Magiczne słowo stworzone przez JavaScript'
 
 
 
+const navigationSwitcher = document.querySelector('.navigation__switcher--js');
+
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visible');
+});
+
+
+
---
+++ js/secound.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+const navigationSwitcher = document.querySelector('.navigation__switcher--js');
+
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigationList = document.querySelector('.navigation__list--js');
+    navigationList.classList.toggle('navigation__list--visible');
+});
+
+
+
